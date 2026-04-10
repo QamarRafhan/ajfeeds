3ce11f40e6fbfd0d0a490ffc6c01f9ee4a226af5
QamarRafhan <qamarrafhan@gmail.com>
2026-04-11 02:36:55 +0500

    H- text changes

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,9 +13,9 @@
         @csrf
     </form>
 
-    <form method="POST" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
+    <form method="POST" action="{{ url('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
-        @method('patch')
+        {{-- @method('patch') --}}
 
         <div class="flex items-center space-x-6">
             <div class="shrink-0">
